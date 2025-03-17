0b6ab0fef46df73dc0bdcc88f15b2e81d324bf87
azak03 <97641168+azak03@users.noreply.github.com>
2025-03-16 23:51:38 -0700

    Add effect for "Designed by Anthony" footer text to type name in with 0's and 1's at random, and to loop that repeatedly.

--- resources/main.js
+++ resources/main.js
@@ -3,13 +3,43 @@ let sections = document.querySelectorAll('section');
 let isCooldown = false;
 
 sections.forEach(section => {
-  section.addEventListener('click', () => {
-    if (isCooldown) return;
-    section.classList.toggle('fullscreen');
-    isCooldown = true;
-    setTimeout(() => {
-      isCooldown = false;
-    }, 1000);
-  });
-});
+    section.addEventListener('click', () => {
+        if (isCooldown) return;
+        section.classList.toggle('fullscreen');
+        isCooldown = true;
+        setTimeout(() => {
+          isCooldown = false;
+        }, 1000);
+      });
+    });
+    let text = "Designed by Anthony";
+    let target = document.getElementById("effect");
+    let index = 0;
+
+    function effect(index = 0) {
+        if (index < text.length) {
+            let randomBinary = Math.random() < 0.5 ? "0" : "1";
+            target.innerHTML += randomBinary;
+
+            setTimeout(() => {
+                target.innerHTML = target.innerHTML.slice(0, -1) + text[index];
+                effect(index + 1);
+            }, 100);
+        } else {
+            setTimeout(() => {
+                eraseEffect(text.length - 1);
+            }, 2000);
+        }
+    }
+
+    function eraseEffect(index) {
+        if (index >= 0) {
+            target.innerHTML = target.innerHTML.slice(0, -1);
+            setTimeout(() => eraseEffect(index - 1), 50);
+        } else {
+            setTimeout(() => effect(0), 500);
+        }
+    }
+
+    effect();
 });
